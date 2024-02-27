328251652c668f694eff5a4c0f01e7d1ef9d5bcf
Christwelve <meng.christian28@gmail.com>
2024-02-27 10:39:47 +0100

    basic service structure

--- hydra-service/src/app.module.ts
+++ hydra-service/src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PromptModule } from './prompt/prompt.module';
+import { MapController } from './map/map.controller';
+import { MapModule } from './map/map.module';
+import { SusController } from './sus/sus.controller';
+import { SusModule } from './sus/sus.module';
 
 @Module({
-  imports: [PromptModule],
-  controllers: [AppController],
+  imports: [PromptModule, MapModule, SusModule],
+  controllers: [AppController, MapController, SusController],
   providers: [AppService],
 })
 export class AppModule {}
